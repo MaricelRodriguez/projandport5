a3e28f296c07ea852dd708b290de7ad027552d12
MRodriguez <msrodriguez@fullsail.edu>
2019-07-30 12:11:34 -0400

    ### Final Development
    - Adds a header to the recipes page.
    - Cleans up the styling on the recipes page.

--- my-app/src/components/recipes/Recipes.js
+++ my-app/src/components/recipes/Recipes.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import './Recipes.css';
+
+import Header from '../header/Header';
+import Btn from '../btn/Btn';
+
 class Recipes extends Component {
   render(){
     return (
-      <div className='flexWrapper'>
-        {this.props.recipes}
-      </div>
+
+        <div className='flexWrapper'>
+          <Header/>
+          <div className='bgWrap'>
+            {this.props.recipes}
+          </div>
+          <Btn size='largeBtn' type='button' text='Back'/>
+        </div>
     );
   }
 }
